3c88be9ac2404c38de7f7ae708dcc001d20cc6f8
Meya05 <majka.dunin_05@wp.pl>
2025-11-16 09:13:40 +0100

    java-password-validation-service

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
-        //write your code here
+    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+        String combinedPassword = password + repeatPassword;
+
+        if (combinedPassword.length() < 10) {
+            // Zmienione z "Błędne hasło" na "Invalid password"
+            throw new PasswordValidationException("Invalid password");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are too short.");
+        }
     }
 
     public void saveUser(User user) {
-        System.out.println("User " + user.toString() + " was saved to database!!!");
+        System.out.println("User " + user.toString() + " has been saved to the database!!!");
     }
 }
